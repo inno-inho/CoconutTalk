8e3a750a0c45b8890a62e1d1398ac839161b2817
inno-inho <zergling2565@gmail.com>
2025-12-25 02:39:26 +0900

    메인 비우기

--- BN/src/main/java/com/example/demo/Repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.entity.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestEntity, Long> {
-}
